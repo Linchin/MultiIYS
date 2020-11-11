ef0309ac2bd1fd2a8334344770432073607caba2
Lingqing Gan <lingqing.gan@gmail.com>
2020-11-10 21:50:08 -0500

    update

--- exp40-real_data_auto_run.py
+++ exp40-real_data_auto_run.py
@@ -11,3 +11,22 @@ run the real data automatically.
 based on exp38
 
 """
+
+import scipy.io as sio
+import os
+from os.path import dirname, join as pjoin
+import numpy as np
+import math
+
+print(dirname)
+
+dir_path = dirname(os.path.realpath(__file__))
+print(dir_path)
+
+
+
+
+
+
+
+
